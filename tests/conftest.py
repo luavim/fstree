e093c826c7aecd4aad1040bdd80b522e6bdcc247
Bogretsov <bogrecov@gmail.com>
2019-10-24 00:02:44 +0300

    Added basic test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,54 @@
+import os
 import pytest
 import pynvim
+import shutil
+import tempfile
 
 
 ARGV = ["nvim", "-u", "init.vim", "--headless", "--embed"]
 
 
 @pytest.fixture
-def nvim(request):
+def nvim(request, testdir):
     nvim = pynvim.attach("child", argv=ARGV)
     request.addfinalizer(nvim.close)
+    nvim.command(f"lcd {testdir}")
     return nvim
+
+@pytest.fixture
+def testdir(request):
+    tmp = tempfile.gettempdir()
+
+    tmp = os.path.join(tmp, "fstree")
+    os.mkdir(tmp)
+
+    os.mkdir(os.path.join(tmp, "d-1"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-1"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-1", "d-1-1-1"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-2"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-3"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-3", "d-1-3-1"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-3", "d-1-3-2"))
+    os.mkdir(os.path.join(tmp, "d-1", "d-1-4"))
+
+    os.mkdir(os.path.join(tmp, "d-2"))
+    os.mkdir(os.path.join(tmp, "d-2", "d-2-1"))
+    os.mkdir(os.path.join(tmp, "d-2", "d-2-2"))
+
+    os.mkdir(os.path.join(tmp, "d-3"))
+
+    os.mkdir(os.path.join(tmp, "d-4"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-1"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-1", "d-4-1-1"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-1", "d-4-1-2"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-2"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-2", "d-4-2-1"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-5"))
+    os.mkdir(os.path.join(tmp, "d-4", "d-4-5", "d-4-5-1"))
+
+    def fin():
+        shutil.rmtree(tmp, ignore_errors=False, onerror=None)
+    
+    request.addfinalizer(fin)
+
+    return tmp
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -7,13 +7,16 @@ BUFOPTS = {
 }
 
 
-def test_open_new(nvim):
+def test_open_new(nvim, testdir):
     nvim.command("FsTreeOpen")
     buf = nvim.buffers[2]
-
-    # import pdb; pdb.set_trace()
 
     for k, v in BUFOPTS.items():
         assert buf.options.get(k) == v
 
-    assert nvim.buffers[2][:] == ["welcome!"]
+    assert buf[:] == [
+        "+ d-1",
+        "+ d-2",
+        "+ d-3",
+        "+ d-4",
+    ]
